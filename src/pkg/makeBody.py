1a69f291051b565048f27bec7e2d83b0aaebe618
Gage Cane <45441614+gagecane@users.noreply.github.com>
2019-06-12 10:34:00 -0700

    Renamed test_keyholder. Added body classes Added body test Added timeline test

--- src/pkg/makeBody.py
+++ src/pkg/makeBody.py
@@ -3,7 +3,6 @@ import json
 # from datetime import datetime
 import emoji
 import os
-from pathlib import Path
 
 
 class KeyHolder:
@@ -98,117 +97,145 @@ class KeyHolder:
         return api
 
 
-def printTweetsToFile(api, user, count, filename, existingBody, max_id=None):
-    """ Print a number of tweets from a specified username to a file
-
-    Arguments:
-        api {tweepy.API(auth)} -- authenticated tweepy API
-        user {string} -- Twitter username with @
-        count {number} -- Number of tweets to print
-        filename {string} -- Output file name
-        existingBody {dict} -- Existing contents of JSON body for this username
-
-    Keyword Arguments:
-        max_id {number} --  Max ID of tweets to retreive.
-                            Will retreive older (lower ID) tweets
-                            (default: {None})
+class OfflineBody:
+    """For interacting with an offline body json
     """
-    tweets = api.user_timeline(
-        # Extended should get tweets longer than 140 but may only
-        # work with api.get_status
-        screen_name=user, count=count, max_id=max_id, include_rts=False)
 
-    OutFileBody = open(filename, "w")
-    if(existingBody is not None):
-        tweetsToWrite = existingBody
-    else:
-        tweetsToWrite = {}
-    for tweet in tweets:
-        tid = tweet.id
-        full_text = tweet.text
+    def __init__(self, filename, outputHtm="test.htm"):
+        self.filename = filename
+        self.outputHtm = outputHtm
+        pass
 
-        # If the tweet is probably truncated, use get_status instead
-        if "…" in full_text:
-            full_text = api.get_status(tid, tweet_mode='extended').full_text
+    def printJsonBody(self, filename):
+        """Generate test.htm from the corpus and open it in a browser
 
-        tweetsToWrite[tid] = emoji.demojize(full_text)
-
-    json.dump(tweetsToWrite, OutFileBody)
-    OutFileBody.close()
-
-
-def printJsonBody(filename):
-    """Generate test.htm from the corpus and open it in a browser
-
-    Arguments:
-        filename {string} -- The input json file to print
-    """
-    with open(filename, 'r') as infile:
-        data = json.load(infile)
-        with open('test.htm', 'w', encoding='utf-8-sig') as f:
-            for d in data.values():
-                f.write((emoji.emojize(d)+"\n").replace("\r\n", "\n")
-                        .replace("\n", "<br />\n"))
-        os.startfile('test.htm')
-
-
-def getJsonBody(filename):
-    """Try loading body from Json file. Return the dict if the file exists
-
-    Arguments:
-        filename {string} -- Json body filename
-
-    Returns:
-        Dict -- Json data or None if file does not exist
-    """
-    try:
+        Arguments:
+            filename {string} -- The input json file to print
+        """
         with open(filename, 'r') as infile:
-            return json.load(infile)
-    except Exception:
-        print(filename + "does not already exist. Creating new file.")
-        Path(filename).touch()
-        return None
+            data = json.load(infile)
+            with open(self.outputHtm, 'w', encoding='utf-8-sig') as f:
+                for d in data.values():
+                    f.write((emoji.emojize(d)+"\n").replace("\r\n", "\n")
+                            .replace("\n", "<br />\n"))
+            os.startfile(self.outputHtm)
 
+    def getJsonBody(self, filename):
+        """Try loading body from Json file. Return the dict if the file exists
 
-def buildBody(username):
-    """Build a body JSON for an input username
+        Arguments:
+            filename {string} -- Json body filename
 
-    Arguments:
-        username {string} -- Twitter handle starting with @
-    """
-    if (not os.path.isdir("./bodies/")):
-        os.mkdir("bodies")
-    outFilename = "./bodies/"+username.replace("@", '')+"_body.json"
-    MaxId = None
-    LastMaxId = -1
-    while(LastMaxId != MaxId):
-        if(MaxId):
-            LastMaxId = MaxId
+        Returns:
+            Dict -- Json data or None if file does not exist
+        """
         try:
-            data = getJsonBody(outFilename)
-            if(data is not None):
-                # Subtract one to not include the same oldest tweet
-                MaxId = min([int(d) for d in data.keys()])-1
-                print("Max ID: %d" % MaxId)
-            else:
-                MaxId = None
-                print("No Max ID yet")
-            printTweetsToFile(
-                api, username, 200, outFilename, data, max_id=MaxId)
-        except Exception as ex:
-            print(ex)
-            break
-    printJsonBody(outFilename)
+            with open(filename, 'r') as infile:
+                return json.load(infile)
+        except Exception:
+            print(filename + "does not exist.")
+            return None
+
+
+class OnlineBody(OfflineBody):
+    """ Requires a connected Timeline.
+        Save tweets from twitter to a body
+    """
+
+    def __init__(self, user, Timeline):
+        self.user = user
+        self.Timeline = Timeline
+        self.outFilename = "./bodies/"+user.replace("@", '')+"_body.json"
+        OfflineBody.__init__(self.outFilename)
+
+    def buildBody(self, user, Timeline):
+        """Build a body JSON for an input user
+
+        Arguments:
+            user {string} -- Twitter handle starting with @
+        """
+        if (not os.path.isdir("./bodies/")):
+            os.mkdir("bodies")
+        MaxId = None
+        LastMaxId = -1
+        while(LastMaxId != MaxId):
+            if(MaxId):
+                LastMaxId = MaxId
+            try:
+                existingData = self.getJsonBody(self.outFilename)
+                if(existingData is not None):
+                    # Subtract one to not include the same oldest tweet
+                    MaxId = min([int(d) for d in existingData.keys()])-1
+                    print("Max ID: %d" % MaxId)
+                else:
+                    MaxId = None
+                    print("No Max ID yet")
+                self.Timeline.printTweetsToFile(
+                    api, user, 200, self.outFilename,
+                    existingData, max_id=MaxId)
+            except Exception as ex:
+                print(ex)
+                break
+        self.printJsonBody(self.outFilename)
+
+
+class Timeline:
+
+    def __init__(self, api):
+        """Class for accessing user timelines. To be used with a Body to save tweets
+
+        Arguments:
+            api {tweepy.API(auth)} -- authenticated tweepy API
+        """
+        self.api = api
+        self.existingBody = {}
+        self.tweetsToWrite = {}
+
+    def printTweetsToFile(self, user, count, filename,
+                          existingBody, max_id=None):
+        """ Print a number of tweets from a specified username to a file
+
+        Arguments:
+            user {string} -- Twitter username with @
+            count {number} -- Number of tweets to print
+            filename {string} -- Output file name
+            existingBody {dict} -- Existing contents
+                                   of JSON body for this username
+
+        Keyword Arguments:
+            max_id {number} --  Max ID of tweets to retreive.
+                                Will retreive older (lower ID) tweets
+                                (default: {None})
+        """
+        self.existingBody = existingBody
+        tweets = self.api.user_timeline(
+            # Extended should get tweets longer than 140 but may only
+            # work with api.get_status
+            screen_name=user, count=count, max_id=max_id, include_rts=False)
+
+        OutFileBody = open(filename, "w")
+        if(existingBody is not None):
+            self.tweetsToWrite = existingBody
+        else:
+            self.tweetsToWrite = {}
+        for tweet in tweets:
+            tid = tweet.id
+            full_text = tweet.text
+
+            # If the tweet is probably truncated, use get_status instead
+            if "…" in full_text:
+                full_text = api.get_status(
+                    tid, tweet_mode='extended').full_text
+
+            self.tweetsToWrite[tid] = emoji.demojize(full_text)
+        json.dump(self.tweetsToWrite, OutFileBody)
+        OutFileBody.close()
 
 
 if __name__ == "__main__":
     keyholder = KeyHolder("consumer_keys.json")
     keyholder.loadKeys()
     api = keyholder.connectToTwitter()
-    username = "@StoobsDeer"
-    buildBody(username)
-    # outFilename = username+"_body%s.json" % datetime.now()
-    #                                       .strftime("%Y%m%d-%H%M%S")
 
-    # for twitter_user in twitter_users:
-    #     downloadTweets(twitter_user)
+    username = "@StoobsDeer"
+    stoobsdeer = OfflineBody('./bodies/StoobsDeer_body.json')
